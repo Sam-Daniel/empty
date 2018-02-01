aec8ca9f86cfe95090f3621dfa8bbd81a1b79c4c
ddany <duminydaniel@gmail.com>
2018-02-02 01:37:44 +0530

    Update add_answer.php

--- add_answer.php
+++ add_answer.php
@@ -13,8 +13,8 @@ $id=$_POST['id'];
 
 // Find highest answer number. 
 $sql="SELECT MAX(a_id) AS Maxa_id FROM $tbl_name WHERE question_id='$id'";
-$result=mysqli_query($conn, $sql)or die("dead"); 
-$rows=mysqli_fetch_array($result);
+$result=mysqli_query($conn, $sql);
+$rows=mysqli_fetch_array($result) or die("dead"); 
 
 
 // add + 1 to highest answer number and keep it in variable name "$Max_id". if there no answer yet set it = 1 
