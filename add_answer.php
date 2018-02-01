a0d7cab26712f89b98e96320ac182094a9002a1e
ddany <duminydaniel@gmail.com>
2018-02-02 02:35:42 +0530

    Update add_answer.php

--- add_answer.php
+++ add_answer.php
@@ -3,7 +3,7 @@ $host="sql12.freesqldatabase.com"; // Host name
 $username="sql12218697"; // Mysql username 
 $password="uJEXQMxpDw"; // Mysql password 
 $db_name="sql12218697"; // Database name 
-$tbl_name="forum_question"; // Table name 
+$tbl_name="forum_answer"; // Table name 
 // Connect to server and select databse.
 $conn = mysqli_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysqli_select_db( $conn, $db_name)or die("cannot select DB");
@@ -14,6 +14,17 @@ $id=$_POST['id'];
 // Find highest answer number. 
 $sql="SELECT MAX(a_id) AS Maxa_id FROM $tbl_name WHERE question_id='$id'";
 $result=mysqli_query($conn, $sql);
+$rows=mysqli_fetch_array($result,MYSQLI_BOTH) or die("Error description: " . mysqli_error($conn));; 
+
+
+// add + 1 to highest answer number and keep it in variable name "$Max_id". if there no answer yet set it = 1 
+if ($rows) {
+$Max_id = $rows['Maxa_id']+1;
+}
+else {
+$Max_id = 1;
+}
+
 
 // get values that sent from form 
 $a_name=$_POST['a_name'];
