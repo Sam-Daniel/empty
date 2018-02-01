93ff4cd887fbca0d017bbf506354abe2354988bd
ddany <duminydaniel@gmail.com>
2018-02-01 16:45:07 +0530

    Update add_topic.php

--- add_topic.php
+++ add_topic.php
@@ -1,8 +1,15 @@
 <?php
 
-include 'index.php';
-include 'connect.php'
+$host="sql12.freesqldatabase.com"; // Host name 
+$username="sql12218697"; // Mysql username 
+$password="uJEXQMxpDw"; // Mysql password 
+$db_name="sql12218697"; // Database name 
+$tbl_name="forum_question"; // Table name 
+// Connect to server and select databse.
+$conn = mysqli_connect("$host", "$username", "$password")or die("cannot connect"); 
+mysqli_select_db( $conn, $db_name)or die("cannot select DB");
 // get data that sent from form 
+
 $topic=$_POST['topic'];
 $detail=$_POST['detail'];
 $name=$_POST['name'];
