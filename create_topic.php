2156ef658f2305590e3e695cc30056c0857a31b4
ddany <duminydaniel@gmail.com>
2018-02-02 16:29:08 +0530

    Update create_topic.php

--- create_topic.php
+++ create_topic.php
@@ -1,40 +1,53 @@
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
 <form id="form1" name="form1" method="post" action="add_topic.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
 <tr>
-<td colspan="3" bgcolor="#E6E6E6"><strong>Create New Topic</strong> </td>
+<td colspan="3" bgcolor="#9b9b9c"><strong>Post Your Question</strong> </td>
 </tr>
 <tr>
-<td width="14%"><strong>Topic</strong></td>
+<td width="14%"><strong>Title</strong></td>
 <td width="2%">:</td>
-<td width="84%"><input name="topic" type="text" id="topic" size="50" /></td>
+<td width="84%"><input name="topic" type="text" id="topic" size="100" /></td>
 </tr>
 <tr>
 <td valign="top"><strong>Detail</strong></td>
 <td valign="top">:</td>
-<td><textarea name="detail" cols="50" rows="3" id="detail"></textarea></td>
+<td><textarea name="detail" cols="100" rows="3" id="detail"></textarea></td>
 </tr>
 <tr>
-<td><strong>Name</strong></td>
-<td>:</td>
-<td><input name="name" type="text" id="name" size="50" /></td>
-</tr>
-<tr>
-<td><strong>Email</strong></td>
-<td>:</td>
-<td><input name="email" type="text" id="email" size="50" /></td>
+
+ 
+
 </tr>
 <tr>
 <td>&nbsp;</td>
 <td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Submit" /> <input type="reset" name="Submit2" value="Reset" /></td>
+<td><input type="submit" class="button-1"<input class = "eff-5" value="Submit" />  <input type="reset" class="button-1" name="Submit2" value="Reset" /></td>
+
 </tr>
 </table>
 </td>
 </form>
 </tr>
 </table>
+<div 
+  </div>
 
-STEP3: Create
+<style>
+ .button-1{
+  right-padding: 5px;
+  width:140px;
+  height:40px;
+  border:2px solid #34495e;
+  background:#ffff;
+  
+  text-align:center;
+  cursor:pointer;
+  position:relative;
+  box-sizing:border-box;
+  overflow:hidden;
+  margin:0 0 40px 0;
+}
+
